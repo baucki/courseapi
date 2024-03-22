998c473ab84a866d9cd466b91aa96908f93fec71
baucki <ibauk6218rn@raf.rs>
2024-03-22 15:16:37 +0100

    final version of the api

--- src/main/java/com/example/courseapi/model/Course.java
+++ src/main/java/com/example/courseapi/model/Course.java
@@ -3,6 +3,7 @@ package com.example.courseapi.model;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 public class Course implements Serializable {
@@ -12,79 +13,81 @@ public class Course implements Serializable {
     private long id;
     private String title;
     private String description;
+    private String location;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private String instructor;
-
     private String imageUrl;
-    private String courseUrl;
-
     public Course() {}
-
-    public Course(long id, String title, String description, String instructor, String imageUrl, String courseUrl) {
+    public Course(long id, String title, String description, String location, LocalDateTime startTime, LocalDateTime endTime, String instructor, String imageUrl) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.instructor = instructor;
         this.imageUrl = imageUrl;
-        this.courseUrl = courseUrl;
     }
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
     public String getInstructor() {
         return instructor;
     }
-
     public void setInstructor(String instructor) {
         this.instructor = instructor;
     }
-
     public String getImageUrl() {
         return imageUrl;
     }
-
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
-
-    public String getCourseUrl() {
-        return courseUrl;
-    }
-
-    public void setCourseUrl(String courseUrl) {
-        this.courseUrl = courseUrl;
-    }
-
     @Override
     public String toString() {
         return "Course{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", start Time='" + startTime + '\'' +
+                ", end Time='" + endTime + '\'' +
                 ", instructor='" + instructor + '\'' +
                 ", imageUrl='" + imageUrl + '\'' +
-                ", courseUrl='" + courseUrl + '\'' +
                 '}';
     }
 }
