052125c195602ca79355cc8f2879a7dafc4ba664
baucki <ibauk6218rn@raf.rs>
2024-04-05 16:26:36 +0200

    adjusting api for working with user

--- src/main/java/com/example/courseapi/model/User.java
+++ src/main/java/com/example/courseapi/model/User.java
@@ -13,10 +13,33 @@ public class User implements Serializable {
     @Column(unique = true)
     private String username;
     private String password;
+    private String name;
+    private String lastName;
+    private String imagePath;
+    private String email;
+    private String country;
+    private String city;
+    private String profession;
+    private String phone;
     public User(){}
-    public User(String username, String password) {
+    public User(String username, String password, String name, String lastName, String imagePath,String email, String country, String city, String profession, String phone) {
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.imagePath = imagePath;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.profession = profession;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
     }
     public String getUsername() {
         return username;
@@ -29,6 +52,54 @@ public class User implements Serializable {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getImagePath() {
+        return imagePath;
+    }
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getProfession() {
+        return profession;
+    }
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
 
